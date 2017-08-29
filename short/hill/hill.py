8820241034714a83f3cbba782ea1676abb1b793d
Austin Gill <notgnoshi@gmail.com>
2017-08-28 21:43:03 -0600

    improve hill cipher, rnadomly works

--- short/hill/hill.py
+++ short/hill/hill.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 import sys
-import numpy
-from sympy.core.numbers import mod_inverse
+import numpy as np
+import gmpy2 as mp
 from math import gcd
 
 ALPHABET_SIZE = 26
-BLOCK_SIZE = 3
+BLOCK_SIZE = 5
 
 
-def gen_key(n=BLOCK_SIZE, N=ALPHABET_SIZE):
-    M = numpy.random.randint(0, ALPHABET_SIZE+1, (n, n))
-    while gcd(int(numpy.linalg.det(M)), ALPHABET_SIZE) != 1:
-        M = numpy.random.randint(0, ALPHABET_SIZE+1, (n, n))
+def gen_key(n, N):
+    """Generate the Hill cipher matrix key"""
+    # Create a random matrix from 0 to N that is nxn
+    M = np.random.randint(0, N+1, (n, n))
+    # The determinant of an integer matrix is an integer
+    while gcd(np.linalg.det(M).astype(int), N) != 1:
+        M = np.random.randint(0, N+1, (n, n))
     return M
 
 
@@ -21,52 +24,68 @@ def numberize(c):
     return ord(c.upper()) - 65
 
 
-def wordize(n):
+def wordize(block):
     """Turns an int into a char"""
-    return chr(n)
+    return ''.join(chr(n + 65) for n in np.nditer(block, ['refs_ok']))
 
 
 def vectorize(message):
     """Turns a string message into a vector of ints where a = 0, b = 1, ..."""
-    return numpy.array([numberize(c) for c in message])
+    return np.matrix([numberize(c) for c in message])
 
 
-def nslice(s, n, truncate=False):
+def nslice(seq, n, truncate=False):
+    """Yield slices of seq, n elements at a time"""
     assert n > 0
-    while len(s) >= n:
-        yield s[:n]
-        s = s[n:]
-    if len(s) and not truncate:
-        yield s
+    while len(seq) >= n:
+        yield seq[:n]
+        seq = seq[n:]
+    if len(seq) and not truncate:
+        yield seq
+
+
+def vmap(func):
+    """Creates a numpy map() function"""
+    return np.vectorize(func)
+
+
+def mround(m):
+    """Rounds a given numpy matrix of GMP reals"""
+    # vmap returns a function to call on the matrix
+    return vmap(mp.rint)(m)
 
 
 def main(message):
-    M = numpy.array([[1, 2, 3], [4, 5, 6], [11, 9, 8]])
-    print(M)
-    d = numpy.linalg.det(M)
-    print(d)
-    M_inv = numpy.linalg.inv(M) * (1/mod_inverse(d, ALPHABET_SIZE)) * d
-    print(M_inv % ALPHABET_SIZE)
+    M = gen_key(BLOCK_SIZE, ALPHABET_SIZE)
+    d = int(np.linalg.det(M))
+    print('d', d)
+    # Compute the matrix inverse mod ALPHABET_SIZE
+    # N = mround(np.linalg.inv(M) * d * mp.invert(d, ALPHABET_SIZE)).astype(int) % ALPHABET_SIZE
+    N = (np.linalg.inv(M) + ALPHABET_SIZE) % ALPHABET_SIZE
+    print(N)
 
     coded_blocks = []
     for block in nslice(message, BLOCK_SIZE):
         nums = vectorize(block)
-        coded_blocks.append((nums * M) % 26)
+        coded_blocks.append(np.matmul(nums, M) % ALPHABET_SIZE)
+    # print('coded blocks:\n\t', coded_blocks)
 
-    print(coded_blocks)
+    cipher = ''
+    for block in coded_blocks:
+        cipher += wordize(block)
+    print('cipher:', cipher)
 
     decoded_blocks = []
     for block in coded_blocks:
-        nums = block * M_inv
-        decoded_blocks.append(nums)
-
-    # print(decoded_blocks)
+        nums = np.matmul(block, N) % ALPHABET_SIZE
+        decoded_blocks.append(nums.astype(int))
+    # print('decoded blocks:\n\t', decoded_blocks)
 
     text = ''
     for block in decoded_blocks:
-        text += ''.join(wordize(c) for c in block)
-    print(text)
+        text += wordize(block)
+    print('text:', text)
 
 
 if __name__ == '__main__':
-    main('hellox')
+    main('hello')
