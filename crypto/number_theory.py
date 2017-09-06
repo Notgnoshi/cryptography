4d9bbd188ce3ee3c55e52dc93a6c6807f0220045
Austin Gill <notgnoshi@gmail.com>
2017-09-06 17:07:36 -0600

    Add more number theory functions

--- crypto/number_theory.py
+++ crypto/number_theory.py
@@ -1,4 +1,6 @@
+import gmpy2
 import math
+import random
 
 
 class NumberTheory(object):
@@ -10,6 +12,19 @@ class NumberTheory(object):
         return iter(filter(lambda x: math.gcd(x, n) == 1, range(n)))
 
     @classmethod
-    def prime_pi(n):
+    def prime_pi(cls, n):
         """Returns the number of primes less than n"""
         return n / math.log(n)
+
+    @classmethod
+    def random_prime(cls, n):
+        """Generates a random large prime number with n digits"""
+        # Generate a random number with n digits
+        num = random.randint(10**(n - 1) + 1, 10**n)
+        # Find the next prime after the number - will *probably* have n digits.
+        return gmpy2.next_prime(num)
+
+    @classmethod
+    def is_prime(cls, x):
+        """Returns True if x is probably prime, False otherwise. Runs 25 Miller-Rabin tests."""
+        return gmpy2.is_prime(x, 25)
