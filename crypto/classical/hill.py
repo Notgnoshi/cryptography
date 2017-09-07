e2931fbc3fcab4e8b4ae17abad941061206b332b
Austin Gill <notgnoshi@gmail.com>
2017-09-06 23:52:25 -0600

    Finished classical Hill Cipher
    * Does not seem to work for large block sizes
    * Or does it not work for larger messages?
    * Either way, it's broken and needs to be fixed

--- crypto/classical/hill.py
+++ crypto/classical/hill.py
@@ -5,8 +5,15 @@ import numpy
 
 
 class HillCipher(object):
+    """Implements a classical Hill Cipher."""
+
+    def __init__(self, key=None, block_size=5, alphabet_size=26):
+        self.key = key if key is not None else self.generate_key(block_size, alphabet_size)
+        self.block_size, _ = key.shape
+        self.alphabet_size = alphabet_size
+
     @classmethod
-    def generate_key(cls, block_size, alphabet_size):
+    def generate_key(cls, block_size, alphabet_size=26):
         """Generate the Hill cipher matrix key"""
         # Create a random matrix from 0 to N that is nxn
         M = numpy.random.randint(0, alphabet_size + 1, (block_size, block_size))
@@ -14,3 +21,74 @@ class HillCipher(object):
         while math.gcd(int(round(numpy.linalg.det(M))), alphabet_size) != 1:
             M = numpy.random.randint(0, alphabet_size + 1, (block_size, block_size))
         return M
+
+    @classmethod
+    def _numberize(cls, c):
+        """Turns a character into an int"""
+        # 65 = A in ascii
+        return ord(c.upper()) - 65
+
+    @classmethod
+    def _characterize(cls, n):
+        """Turns an int into a character"""
+        return chr(n + 65)
+
+    @classmethod
+    def encode(cls, string):
+        """Encodes an alphabetic string such that A:0, B:1, C:2, ..."""
+        return [cls._numberize(c) for c in string.upper()]
+
+    @classmethod
+    def decode(cls, nums):
+        """Decodes a numeric list such that 0:A, 1:B, 2:C, ..."""
+        return ''.join(cls._characterize(n) for n in nums).upper()
+
+    @classmethod
+    def _nslice(cls, seq, n, truncate=False):
+        """Yield slices of seq, n elements at a time"""
+        assert n > 0
+        while len(seq) >= n:
+            yield seq[:n]
+            seq = seq[n:]
+        if len(seq) and not truncate:
+            yield seq
+
+    @classmethod
+    def pad_message(cls, message, block_size):
+        """Pads a message with enough 'X's to produce full blocks."""
+        # If the message length is evenly divisible by the block size, don't pad.
+        if len(message) % block_size == 0:
+            return message
+
+        return message + 'X' * (block_size - (len(message) % block_size))
+
+    def encrypt(self, message):
+        """Encrypts a the given message using the Hill Cipher."""
+        message = self.pad_message(message, self.block_size)
+
+        # Produce the coded blocks
+        coded_blocks = []
+        for block in self._nslice(message, self.block_size):
+            nums = numpy.matrix(self.encode(block))
+            coded_blocks.append(numpy.mod(numpy.matmul(nums, self.key), self.alphabet_size))
+
+        cipher = ''
+        for block in coded_blocks:
+            cipher += self.decode(numpy.nditer(block))
+
+        return cipher
+
+    def decrypt(self, ciphertext):
+        """Decrypts the given ciphertext using the Hill Cipher."""
+        m_inv = Math.modular_matrix_inverse(self.key, self.alphabet_size)
+
+        decoded_blocks = []
+        for block in self._nslice(ciphertext, self.block_size):
+            nums = numpy.matrix(self.encode(block))
+            decoded_blocks.append(numpy.mod(numpy.matmul(nums, m_inv), self.alphabet_size))
+
+        text = ''
+        for block in decoded_blocks:
+            text += self.decode(numpy.nditer(block))
+
+        return text
--- tests/test_hill.py
+++ tests/test_hill.py
@@ -1,1 +1,54 @@
-from crypto import HillCipher
+from crypto import HillCipher, Passwords
+import numpy
+from string import ascii_uppercase
+import unittest
+
+
+class HillCipherTest(unittest.TestCase):
+    def test_encode(self):
+        self.assertListEqual(list(range(0, 26)), HillCipher.encode(ascii_uppercase))
+
+    def test_decode(self):
+        self.assertEqual(ascii_uppercase, HillCipher.decode(list(range(0, 26))))
+
+    def test_pad_message(self):
+        message = 'test'
+        block_size = 5
+        self.assertEqual('testX', HillCipher.pad_message(message, block_size))
+        block_size = 6
+        self.assertEqual('testXX', HillCipher.pad_message(message, block_size))
+        block_size = 7
+        self.assertEqual('testXXX', HillCipher.pad_message(message, block_size))
+        message = 'testxxxtest'
+        self.assertEqual('testxxxtestXXX', HillCipher.pad_message(message, block_size))
+
+    def test_generate_key(self):
+        block_size = 7
+        key = HillCipher.generate_key(block_size)
+        cipher = HillCipher(key)
+        self.assertListEqual(key.tolist(), cipher.key.tolist())
+        self.assertEqual(block_size, key.shape[0])
+        self.assertEqual(block_size, cipher.block_size)
+
+    def test_encrypt(self):
+        key = numpy.matrix([[15, 12], [11, 3]])
+        hill = HillCipher(key)
+        message = 'howareyoutodayx'
+        expected = 'ZWSENIUSPLJVEUAH'
+        self.assertEqual(expected, hill.encrypt(message))
+
+    def test_decrypt(self):
+        key = numpy.matrix([[15, 12], [11, 3]])
+        hill = HillCipher(key)
+        expected = 'HOWAREYOUTODAYXX'
+        cipher = 'ZWSENIUSPLJVEUAH'
+        self.assertEqual(expected, hill.decrypt(cipher))
+
+    def test_random_key(self):
+        block_size = 7
+        key = HillCipher.generate_key(block_size)
+        # Generate random 49 character string
+        message = Passwords.gen_alpha(49)
+        hill = HillCipher(key)
+        cipher = hill.encrypt(message)
+        self.assertEqual(message, hill.decrypt(cipher))
