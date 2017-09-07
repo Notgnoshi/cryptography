1306db3de096ec6a9cd6a9f3e0c57f669748b215
Austin Gill <notgnoshi@gmail.com>
2017-09-06 21:45:55 -0600

    Improve symbol frequency calculator
    * Add official string representation
    * subclass Counter (which subclasses dict)
    * Improve symbol frequency script

--- crypto/frequencies.py
+++ crypto/frequencies.py
@@ -1,11 +1,23 @@
 from collections import Counter
+from operator import itemgetter
 
 
 class SymbolFrequencies(Counter):
     """Calculates a symbol frequency table from a given iterable"""
+
     def __init__(self, iterable, filter_key=str.isalpha):
+        # Call the parent Counter constructor.
         super().__init__(filter(filter_key, iterable))
-        total = sum(self.values())
-        # Allow same syntax for counts as for proportions
+        # Total number of symbols.
+        self.total = sum(self.values())
+        # Allow same syntax for counts as for proportions.
         self.counts = self
-        self.proportions = {key: value / total for key, value in self.items()}
+        # Calculate the proportions of each symbol.
+        self.proportions = {key: value / self.total for key, value in self.items()}
+
+    def __repr__(self):
+        """The official string representation of the frequency table"""
+        rep = 'symbol:\tcount:\tproportion:\n'
+        for key, value in sorted(self.items(), key=itemgetter(1), reverse=True):
+            rep += '{}\t{}\t{}\n'.format(key, value, value / self.total)
+        return rep
--- short/frequencies/frequencies.py
+++ short/frequencies/frequencies.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
-from collections import Counter
-from operator import itemgetter
 import sys
-
-if sys.version_info[0] < 3:
-    raise EnvironmentError("Python 3+ is required.")
-
-
-def alphabet(text):
-    """Return only English alphabetic characters of given text"""
-    with open(text, 'r') as f:
-        # Project Gutenberg encodes its documents with utf-8, and some books use diacritic marks
-        # Avoid encoding as ascii and then decoding back into text if unicode alphabetic characters
-        # are acceptable.
-        s = ''.join(line.strip() for line in f).encode('ascii', errors='ignore').decode().upper()
-        return filter(str.isalpha, s)
+sys.path.append('../../')
+from crypto import SymbolFrequencies
 
 
 def main(texts):
     """Print the letter frequency table for the given files."""
     for text in texts:
-        count = Counter(alphabet(text))
-        print(80*'=')
-        print('file:', text)
-        print(80*'=')
-        print('\tsymbol:\tcount:\tfraction:')
-        total = sum(count.values())
-        for key, value in sorted(count.items(), key=itemgetter(1), reverse=True):
-            print('\t{}\t{}\t{:.3f}'.format(key, value, value / total))
+        with open(text, 'r') as f:
+            count = SymbolFrequencies(''.join(f).upper())
+            print(80 * '=')
+            print('file:', text)
+            print(80 * '=')
+            print(count)
 
 
 if __name__ == '__main__':
-    if '-h' in sys.argv or '--help' in sys.argv:
+    if '-h' in sys.argv or '--help' in sys.argv or len(sys.argv) < 2:
         print(main.__doc__)
-        print('Usage: {} <text 1> <text 2> ...')
-    elif len(sys.argv) < 2:
-        print('Usage: {} <text 1> <text 2> ...')
+        print('Usage: {} <text 1> <text 2> ...'.format(sys.argv[0]))
     else:
         main(sys.argv[1:])
