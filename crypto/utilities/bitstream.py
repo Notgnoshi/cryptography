349fe0f72f31f0cc35f19228f1cceaad9d6e2fbe
Austin Gill <notgnoshi@gmail.com>
2017-09-11 19:55:49 -0600

    add bitstream/bitfield utilities

---
+++ crypto/utilities/bitstream.py
@@ -0,0 +1,54 @@
+class Bitfield(object):
+    """A utility class for bitfield manipulations"""
+    @classmethod
+    def set_ith_bit(cls, num, index, value):
+        """Sets the ith bit of num to the given value"""
+        # https://stackoverflow.com/questions/28360586/setting-nth-bit-of-unsigned-int
+        num ^= (-value ^ num) & (1 << index)
+        return num
+
+    @classmethod
+    def set_bit(cls, num, bit):
+        """Sets to ith bit of num to 1"""
+        return cls.set_ith_bit(num, bit, 1)
+
+    @classmethod
+    def clear_bit(cls, num, bit):
+        """Sets the ith bit of num to 0"""
+        return cls.set_ith_bit(num, bit, 0)
+
+    @classmethod
+    def get_bit(cls, num, bit):
+        """Gets the value of the ith bit of num"""
+        return bool((num >> bit) & 1)
+
+    @classmethod
+    def bits(cls, value, bits=None):
+        """A generator to iterate over `bits` bits of the given value"""
+        bits = value.bit_length() if bits is None else bits
+        for i in range(bits):
+            yield (value >> i) & 1
+
+    @classmethod
+    def binary_string(cls, string):
+        """Returns the binary representation of a string, lsbf"""
+        return ''.join('{0:08b}'.format(ord(x), 'b')[::-1] for x in string)
+
+
+class Bitstream(object):
+    """Turns an ascii string into a bit-by-bit bitstream of its lsbf binary representation."""
+    def __init__(self, string):
+        self.bitstream = self._bits(bytes(string, 'ascii'))
+
+    def _bits(self, bytes):
+        """A generator to yield bit after bit of the bitstream"""
+        for byte in bytes:
+            for bit in Bitfield.bits(byte, 8):
+                yield bit
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """Get the next bit from the bitstream"""
+        return next(self.bitstream)
---
+++ tests/test_bitstream.py
@@ -0,0 +1,66 @@
+from crypto.utilities import Bitfield, Bitstream
+from crypto.random import generate_alpha
+import unittest
+
+
+class BitfieldTest(unittest.TestCase):
+    byte_array = b'\x53\x61\x6c\x74\x65\x64\x5f\x5f\x1b\x73\xa1\x62'
+
+    def test_bit_generator1(self):
+        bits = []
+        for bit in Bitfield.bits(0b10101100):
+            bits.append(bit)
+
+        self.assertListEqual(bits, [0, 0, 1, 1, 0, 1, 0, 1])
+
+    def test_bit_generator2(self):
+        bits = []
+        for bit in Bitfield.bits(self.byte_array[0], bits=4):
+            bits.append(bit)
+
+        self.assertListEqual(bits, [1, 1, 0, 0])
+
+    def test_bit_generator3(self):
+        bits = []
+        for bit in Bitfield.bits(0x6153, bits=14):
+            bits.append(bit)
+
+        self.assertListEqual(bits, [1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1])
+
+    def test_get_bit(self):
+        val = 0b10101100
+        bits = [Bitfield.get_bit(val, i) for i in range(8)]
+        self.assertListEqual(bits, [0, 0, 1, 1, 0, 1, 0, 1])
+
+    def test_set_bit(self):
+        val = 0b10101100
+        new_val = Bitfield.set_bit(val, 0)
+        new_val = Bitfield.set_bit(new_val, 1)
+
+        self.assertEqual(new_val, 0b10101111)
+
+    def test_clear_bit(self):
+        val = 0b10101100
+        new_val = Bitfield.clear_bit(val, 2)
+        new_val = Bitfield.clear_bit(new_val, 3)
+
+        self.assertEqual(new_val, 0b10100000)
+
+
+class BitstreamTest(unittest.TestCase):
+    def test_bitstream_1(self):
+        string = 'abcd'
+        bitstream = Bitstream(string)
+        # abcd in binary lsbf format
+        expected = [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0,
+                    0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0]
+
+        actual = list(bit for bit in bitstream)
+        self.assertListEqual(actual, expected)
+
+    def test_bitstream_2(self):
+        string = generate_alpha(1000)
+        bitstream = Bitstream(string)
+        actual = ''.join(str(bit) for bit in bitstream)
+        expected = Bitfield.binary_string(string)
+        self.assertSequenceEqual(actual, expected)
