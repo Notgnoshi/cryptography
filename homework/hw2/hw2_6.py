28b92b22e4db119a2dedf2edd237735554981110
Austin Gill <notgnoshi@gmail.com>
2017-10-03 15:52:59 -0600

    Change primite root implementation to use coprimes of m

--- homework/hw2/hw2_6.py
+++ homework/hw2/hw2_6.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+import sys
+sys.path.append('../../')
+from crypto.math import coprimes
 from gmpy2 import powmod
 
 
 def primitive_roots(m):
     """Yields the primitive roots of `m`"""
-    for a in range(1, m):
-        if set(powmod(a, p, m) for p in range(1, m)) == set(range(1, m)):
+    for a in coprimes(m):
+        if set(powmod(a, p, m) for p in range(1, m)) == set(coprimes(m)):
             yield a
 
 
 if __name__ == '__main__':
-    print(list(primitive_roots(11)))
+    print(list(primitive_roots(int(sys.argv[1]))))
