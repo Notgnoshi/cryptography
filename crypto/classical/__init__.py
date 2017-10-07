90d6b41a092206a25a28a6eaae6e098c36e45bf5
Austin Gill <notgnoshi@gmail.com>
2017-10-07 09:25:27 -0600

    Fully remove ADFGX cipher
    * Fixes unit test crash

--- crypto/classical/__init__.py
+++ crypto/classical/__init__.py
@@ -2,7 +2,6 @@
 Classical cipher submodule of crypto. Defines several classical ciphers.
 """
 
-from .adfgx import AdfgxCipher
 from .affine import AffineCipher
 from .hill import HillCipher
 from .lfsr import LfsrCipher
