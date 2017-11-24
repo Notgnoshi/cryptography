7364d34b83a9e4dc7cc88ff9a809b1291a078096
Austin Gill <notgnoshi@gmail.com>
2017-11-24 13:08:29 -0700

    Improve AffineCipher implementation and documentation
    * Add more usage examples
    * Remove private methods
    * Remove unnecessary complexity
    * Add more descriptive documentation

--- crypto/classical/affine.py
+++ crypto/classical/affine.py
@@ -6,38 +6,90 @@ class AffineCipher(object):
     """
         Implements a classical Affine Cipher.
 
+        Affine ciphers implements the encryption function Encrypt : plain -> cipher defined by some
+        affine multiplication and shift:
+            E(m_i) = a * m_i + b (mod 26)
+
+        Note that for this encryption to be reversible, the multiplicative inverse (mod 26) of a
+        must exist. Decryption is implemented as the function Decryption : cipher -> plain defined
+        by
+            D(c_i) = a_inv * (c_i - b) (mod 26)
+
         Example usage:
 
         >>> cipher = AffineCipher(9, 18)
         >>> cipher.encrypt('affine')
         'sllmfc'
+        >>> cipher.decrypt('sllmfc')
+        'affine'
     """
 
     def __init__(self, a, b):
+        """
+            Constructs an AffineCipher object with the parameters `a` and `b`. Note that `a` must
+            have a multiplicative inverse mod 26.
+
+            Example:
+
+            >>> cipher = AffineCipher(2, 15)
+            Traceback (most recent call last):
+              File "<stdin>", line 1, in ?
+            ValueError: 2 must be invertible mod 26
+            >>> cipher = AffineCipher(9, 18)
+        """
+        self.modulus = 26
         self.a = a
         self.b = b
-        self.a_inverse = gmpy2.invert(a, 26)
+        try:
+            self.a_inverse = gmpy2.invert(a, self.modulus)
+        except ZeroDivisionError:
+            raise ValueError('{} must be invertible mod {}'.format(self.a, self.modulus))
 
-    def _encrypt_chr(self, character):
-        """Numerically encrypts a single given character."""
-        return (self.a * int_mapping(character) + self.b) % 26
+    def encrypt_chr(self, character):
+        """
+            Encrypts a single given character by first mapping that character to a number in the
+            range 0..25 before numerically encrypting it and mapping it back to a character.
 
-    def _encrypt_str(self, message):
-        """Numerically encrypts a given message, returning a generator of encrypted numbers."""
-        return (self._encrypt_chr(character) for character in message)
+            Example:
 
-    def _decrypt_chr(self, character):
-        """Numerically decrypts a single given character."""
-        return self.a_inverse * (int_mapping(character) - self.b) % 26
+            >>> cipher = AffineCipher(9, 18)
+            >>> cipher.encrypt_chr('a')
+            's'
+        """
+        return char_mapping((self.a * int_mapping(character) + self.b) % self.modulus)
 
-    def _decrypt_str(self, cipher):
-        """Numerically decrypts a given message, returning a generator of decrypted numbers."""
-        return (self._decrypt_chr(character) for character in cipher)
+    def decrypt_chr(self, character):
+        """
+            Decrypts a single given character by first mapping that character to a number in the
+            range 0..25 before numerically decrypting it and mapping it back to a character.
+
+            Example:
+            >>> cipher = AffineCipher(9, 18)
+            >>> cipher.decrypt_chr('s')
+            'a'
+        """
+        return char_mapping(self.a_inverse * (int_mapping(character) - self.b) % self.modulus)
 
     def encrypt(self, message):
-        """Textually encrypts a given message."""
-        return ''.join(char_mapping(num) for num in self._encrypt_str(preprocess(message)))
+        """
+            Textually encrypts a given message.
+
+            Example:
+
+            >>> cipher = AffineCipher(9, 18)
+            >>> cipher.encrypt('affine')
+            'sllmfc'
+        """
+        return ''.join(self.encrypt_chr(character) for character in preprocess(message))
 
     def decrypt(self, cipher):
-        """Textually decrypts a given ciphertext."""
-        return ''.join(char_mapping(num) for num in self._decrypt_str(cipher))
+        """
+            Textually decrypts a given ciphertext.
+
+            Example:
+
+            >>> cipher = AffineCipher(9, 18)
+            >>> cipher.decrypt('sllmfc')
+            'affine'
+        """
+        return ''.join(self.decrypt_chr(character) for character in cipher)
