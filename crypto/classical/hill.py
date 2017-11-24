82f15bd4412b9b345ce06c43219a0057814c406f
Austin Gill <notgnoshi@gmail.com>
2017-11-24 13:39:12 -0700

    Improve HillCipher documentation

--- crypto/classical/hill.py
+++ crypto/classical/hill.py
@@ -5,9 +5,24 @@ from crypto.math import modular_matrix_inverse
 
 
 class HillCipher(object):
-    """Implements a classical Hill Cipher."""
+    """
+        Implements a classical Hill Cipher.
+
+        The Hill Cipher is one of the first examples of a block cipher, where a one character change
+        in the input produces more than one character change in the output and vice versa.
+
+        The Hill Cipher works by splitting the given message into blocks which it then multiplies by
+        the key matrix to encrypt the message.
+
+        Decryption works much the same way, except we use the key matrix's modular_matrix_inverse
+        to multiply each cipehrtext block.
+    """
 
     def __init__(self, key=None, block_size=5, alphabet_size=26):
+        """
+            Constructs a HillCipher object from an optional key and block size. Will generate its
+            own random key if one is not given. The key may be accessed as the `self.key` property.
+        """
         # Generate a key if one is not given.
         self.key = key if key is not None else self.generate_key(block_size, alphabet_size)
         # Infer the block_size if the key was passed in.
@@ -18,7 +33,17 @@ class HillCipher(object):
 
     @staticmethod
     def generate_key(block_size, alphabet_size=26):
-        """Generate the Hill cipher matrix key"""
+        """
+            Generate the Hill cipher matrix key by generating random matrices until it finds one
+            with a determinant invertible mod the alphabet_size.
+
+            This method hangs when given large block sizes. The largest usable block size seems
+            to be 11.
+
+            Example:
+
+            >>> key = HillCipher.generate_key(4)
+        """
         # Create a random matrix from 0 to N that is nxn
         M = numpy.random.randint(0, alphabet_size + 1, (block_size, block_size))
         # The determinant of an integer matrix is an integer
@@ -29,16 +54,39 @@ class HillCipher(object):
 
     @staticmethod
     def encode(string):
-        """Encodes an alphabetic string such that a:0, b:1, c:2, ..."""
+        """
+            Encodes an alphabetic string such that a:0, b:1, c:2, ...
+
+            Example:
+
+            >>> HillCipher.encode('abc')
+            [0, 1, 2]
+        """
         return [int_mapping(c) for c in string]
 
     @staticmethod
     def decode(nums):
-        """Decodes a numeric list such that 0:a, 1:b, 2:c, ..."""
+        """
+            Decodes a numeric list such that 0:a, 1:b, 2:c, ...
+
+            Example:
+
+            >>> HillCipher.decode([0, 1, 2])
+            'abc'
+        """
         return ''.join(char_mapping(int(n)) for n in nums)
 
     def encrypt(self, message):
-        """Encrypts a the given message using the Hill Cipher."""
+        """
+            Encrypts a the given message using the Hill Cipher.
+
+            Example:
+
+            >>> key = numpy.matrix([[15, 12], [11, 3]])
+            >>> cipher = HillCipher(key)
+            >>> cipher.encrypt('howareyoutodayx')
+            'zwseniuspljveuah'
+        """
         blocks = []
         # Convert the message into blocks with the given fill value to make them evenly divisible.
         for block in nslice(preprocess(message), self.block_size, self.fill_value):
@@ -53,7 +101,16 @@ class HillCipher(object):
         return ciphertext
 
     def decrypt(self, ciphertext):
-        """Decrypts the given ciphertext using the Hill Cipher."""
+        """
+            Decrypts the given ciphertext using the Hill Cipher.
+
+            Example:
+
+            >>> key = numpy.matrix([[15, 12], [11, 3]])
+            >>> cipher = HillCipher(key)
+            >>> cipher.decrypt('zwseniuspljveuah')
+            'howareyoutodayxx'
+        """
         blocks = []
         # Encrypted text should have evenly divisible blocks, but if not, avoid a crash by filling.
         for block in nslice(ciphertext, self.block_size, self.fill_value):
