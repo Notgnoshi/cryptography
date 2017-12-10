5da1ed95d262639d8204f44b81ced12e04c3ca33
Austin Gill <notgnoshi@gmail.com>
2017-12-10 16:52:19 -0700

    change to MSBF
    Book lists binary numbers in MSBF form, while all of my bitstream
    functions use LSBF. If not careful, bitey ass.

--- crypto/ciphers/toy_des.py
+++ crypto/ciphers/toy_des.py
@@ -8,7 +8,7 @@ class ToyDesCipher(object):
     """
         Implements a toy version of the DES cipher as described in class and the textbook.
 
-        On each round, L_i and R_i are 6 bits, while K is 8 bits.
+        On each round, L_i and R_i are 6 bits, while K_i is 8 bits.
     """
 
     # The (in)famous S-boxes
@@ -19,15 +19,25 @@ class ToyDesCipher(object):
 
     def __init__(self, key, number_of_rounds=4):
         """
-            Takes the first 9 bits of the given `key` and runs `number_of_rounds` of the Feistel
+            Takes the lowest 9 bits of the given `key` and runs `number_of_rounds` of the Feistel
             System on the messages to be encrypted.
         """
-        self.key = list(bits_of(key, 9))
+        # bits_of yields bits LSBF, want to store MSBF to be consistent with the book
+        self.key = tuple(bits_of(key, 9))[::-1]
         self.number_of_rounds = number_of_rounds
 
     @classmethod
     def expand_bits(cls, bitstring):
-        """Expands a 6 - bit bitstring into an 8 - bit bitstring"""
+        """
+            Expands a 6 - bit bitstring into an 8 - bit bitstring
+
+            Example:
+
+            >>> ToyDesCipher.expand_bits([0, 1, 1, 0, 0, 1])
+            [0, 1, 0, 1, 0, 1, 0, 1]
+            >>> ToyDesCipher.expand_bits([1, 0, 0, 1, 1, 0])
+            [1, 0, 1, 0, 1, 0, 1, 0]
+        """
         if len(bitstring) != 6:
             raise ValueError('`bitstring` must be 6 bits')
         else:
@@ -35,12 +45,24 @@ class ToyDesCipher(object):
 
     @classmethod
     def S1(cls, bits):
-        """Returns the S1-box value associated with the given bits"""
+        """
+            Returns the S1-box value associated with the given bits
+
+            Example:
+            >>> ToyDesCipher.S1((1, 1, 1, 1))
+            (0, 1, 1)
+        """
         return cls._S1[bits[0]][bits_to_integer(reversed(bits[1:]))]
 
     @classmethod
     def S2(cls, bits):
-        """Returns the S2-box value associated with the given bits"""
+        """
+            Returns the S2-box value associated with the given bits
+
+            Example:
+            >>> ToyDesCipher.S2((1, 1, 1, 1))
+            (1, 0, 0)
+        """
         return cls._S2[bits[0]][bits_to_integer(reversed(bits[1:]))]
 
     @classmethod
@@ -60,29 +82,57 @@ class ToyDesCipher(object):
     @classmethod
     def rotate(cls, key, i):
         """
-            The ToyDesCipher key rotation function as defined in the book.
+            The ToyDesCipher key rotation function as defined in the book. Note that the given index
+            is one-indexed, not zero.
 
             Example:
 
             >>> ToyDesCipher.rotate([0, 1, 0, 0, 1, 1, 0, 0, 1], 4)
             [0, 1, 1, 0, 0, 1, 0, 1]
+            >>> ToyDesCipher.rotate([0, 1, 0, 0, 1, 1, 0, 0, 1], 3)
+            [0, 0, 1, 1, 0, 0, 1, 0]
             >>> ToyDesCipher.rotate([0, 0, 1, 0, 0, 1, 1, 0, 1], 2)
             [0, 1, 0, 0, 1, 1, 0, 1]
+            >>> ToyDesCipher.rotate([0, 0, 1, 0, 0, 1, 1, 0, 1], 1)
+            [0, 0, 1, 0, 0, 1, 1, 0]
         """
         if i == 1:
-            return key[:-1]
-
-        return key[i - 1:] + key[:i - 2]
+            return key[:8]
+        i -= 1
+        return key[i:] + key[:i - 1]
 
     def feistel_round(self, L, R, i):
-        """Runs one round of the Feistel System on the given chunk"""
+        """
+            Runs one round of the Feistel System on the given chunk
+
+            Example:
+
+            >>> cipher = ToyDesCipher(0b010011001)
+            >>> cipher.feistel_round((0, 1, 1, 1, 0, 0), (1, 0, 0, 1, 1, 0), 4)
+            ((1, 0, 0, 1, 1, 0), (0, 1, 1, 0, 0, 0))
+        """
         K = self.rotate(self.key, i)
         return R, tuple(xor_streams(L, self.f(R, K)))
 
-    def encrypt_chunk(self, chunk):
-        """Runs the Feistel System rounds on a single (L, R) chunk to encrypt it."""
+    def encrypt_chunk(self, chunk, rounds=None):
+        """
+            Runs the Feistel System rounds on a single (L, R) chunk to encrypt it.
+
+            Will accept a list of round numbers to enable starting in the middle of the algorithm.
+
+            Example:
+            >>> cipher = ToyDesCipher(0b001001101)
+            >>> L1R1 = ((0, 0, 0, 1, 1, 1), (0, 1, 1, 0, 1, 1))
+            >>> L1R1S = ((1, 0, 1, 1, 1, 0), (0, 1, 1, 0, 1, 1))
+            >>> cipher.encrypt_chunk(L1R1, rounds=[2, 3, 4])
+            ((0, 0, 0, 0, 1, 1), (1, 0, 0, 1, 0, 1))
+            >>> cipher.encrypt_chunk(L1R1)  # Treat L1R1 as L0R0 and run all four rounds.
+            ((0, 1, 0, 1, 1, 1), (0, 0, 0, 1, 0, 0))
+            >>> cipher.encrypt_chunk(L1R1S, rounds=[2, 3, 4])
+            ((1, 0, 0, 1, 0, 0), (0, 1, 1, 0, 0, 0))
+        """
         L, R = chunk
-        for i in range(1, self.number_of_rounds + 1):
+        for i in range(1, self.number_of_rounds + 1) if rounds is None else rounds:
             L, R = self.feistel_round(L, R, i)
         return L, R
 
